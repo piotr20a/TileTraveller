3c6c729e09338772fabe2665fbe96c7f0290b924
Piotr Abramek <piotr20@ru.is>
2020-09-17 10:48:59 +0000

    Victory and message status

--- tile_traveller.py
+++ tile_traveller.py
@@ -5,22 +5,22 @@
     At each iteration, the player enters the first letter of the direction he/she wishes to travel,
     after which the player should be located in another tile and the options for the new tile are then printed out.
 '''
-def display_available_paths(current_tile):
+def find_available_paths(current_tile):
     if current_tile == 1 or current_tile == 4 or current_tile == 7:
-        return "N"
+        return "You can travel: (N)orth."
     if current_tile == 2:
-        return "N, E, S"
+        return "You can travel: (N)orth or (E)ast or (South)."
     if current_tile == 3:
-        return "E, S"
+        return "You can travel: (E)ast or (S)outh."
     if current_tile == 5 or current_tile == 9:
-        return "W, S"
+        return "You can travel: (W)est or (S)outh."
     if current_tile == 6:
-        return "E, W"
+        return "You can travel: (E)ast or (W)est."
     if current_tile == 8:
-        return "N, S"
+        return "You can travel: (N)orth or (S)outh."
 
 def switch_tile(current_tile, direction):
-    available_paths = display_available_paths(current_tile)
+    available_paths = find_available_paths(current_tile)
     if direction.upper() in available_paths:
         if direction.upper() == "N":
             current_tile += 1
@@ -33,13 +33,15 @@ def switch_tile(current_tile, direction):
         return current_tile
     else:
         print("Not a valid direction!")
-    
+
 # prufa
 current_tile = 1
 while current_tile != 7:
     print("You are in tile", current_tile)
-    print("You can travel:", display_available_paths(current_tile)+".")
+    print("You can travel:", find_available_paths(current_tile))
 
     direction = input("Direction: ")
     current_tile = switch_tile(current_tile, direction)
+
+print("Victory!")
 
